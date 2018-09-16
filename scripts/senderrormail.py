1e6d82e7fb583d294b7b519bef2c8089979b71da
István Váradi <ivaradi@gmail.com>
2018-09-16 08:23:58 +0200

    Updated mail sender script

--- scripts/senderrormail.py
+++ scripts/senderrormail.py
@@ -7,8 +7,8 @@ import smtplib
 if __name__ == "__main__":
     errorCode = int(sys.argv[1])
 
-    msg = "From: eBUS démon <ebus@mezga.varadiistvan.hu>\r\n"
-    msg += "To: ivaradi@varadiistvan.hu\r\n"
+    msg = "From: eBUS démon <ebus@superhost.com>\r\n"
+    msg += "To: ivaradi@mail.superhost.com\r\n"
     msg += "Subject: Kazán hiba: %03d\r\n" % (errorCode,)
     msg += "\r\n"
     msg += "Üdvözletem!\r\n"
@@ -18,9 +18,9 @@ if __name__ == "__main__":
     msg += "Az eBUS démon\r\n"
 
     server = smtplib.SMTP_SSL()
-    server.connect("mail.varadiistvan.hu", 10025)
+    server.connect("mail.superhost.com", 25)
     #server.set_debuglevel(9)
-    server.sendmail("ebus@mezga.varadiistvan.hu",
-                    ["ivaradi@varadiistvan.hu"],
+    server.sendmail("ebus@superhost.com",
+                    ["ivaradi@mail.superhost.com"],
                     msg)
     server.quit()
